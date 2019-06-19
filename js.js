584680aa628b69324f8ac1a74068f7409470ebcf
andreikrivets <etopushka@gmail.com>
2019-06-19 10:08:13 +0300

    nalog

--- js.js
+++ js.js
@@ -1,48 +1,58 @@
-function parse() {
+﻿function parse() {
+    if (document.querySelector('table')) {
+        document.querySelector('table').innerHTML = '';
+    }
     const NMtext = document.querySelector('.input-area').value;
     const Match = NMtext.match(/[0-9]{9}/g);
     request(Match);
 }
 
 let c = 1;
+let R;
 function request(arr) {
-    let R;
-    const url = 'http://egr.gov.by/egrn/API.jsp?';
+    const url = 'https://www.portal.nalog.gov.by/grp/getData?';
+    const type = '&charset=UTF-8&type=json'
     constructor('table', 'result-table', '.container', '', 0);
     constructor('tr', 'tr-header', '.result-table', '', 0);
     constructor('td', 'td-header', '.tr-header', '<p>элемент</p>', 0);
+    constructor('td', 'td-header', '.tr-header', '<p>лицо</p>', 0);
     constructor('td', 'td-header', '.tr-header', '<p>активность</p>', 0);
     constructor('td', 'td-header', '.tr-header', '<p>дата регистрации</p>', 0);
     constructor('td', 'td-header', '.tr-header', '<p>дата исключения</p>', 0);
     arr.forEach((e) => {
-        const fullUrl = url + 'NM=' + e;
+        const fullUrl = url + 'unp=' + e + type;
+        console.log(fullUrl);
         fetch(fullUrl, { mode: 'cors' }).then((resp) => {
             return resp.text();
         })
             .then((text) => {
-                let R = JSON.parse(text);
-                let date;
-                let state;
-                if (R[0].NS) {
-                    state = 'активен';
-                } else if (!R[0].NS) {
-                    state = 'не активен';
+                if (text) {
+                    let R = JSON.parse(text);
+                    let date;
+                    console.log(R);
+                    if (R.ROW.DD == null) {
+                        date = '-';
+                    } else {
+                        date = R.ROW.DD;
+                    }
+                    constructor('tr', `tr-content${c}`, '.result-table', '', 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `${R.ROW.VUNP}`, 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `${R.ROW.VNAIMK}`, 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `${R.ROW.VKODS}`, 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `${R.ROW.DREG}`, 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `${date}`, 0);
+                    c++;
                 }
-                if (R[0].DD == null) {
-                    date = '-';
-                } else {
-                    date = R[0].DD;
+                else {
+                    constructor('tr', `tr-content${c}`, '.result-table', '', 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `${e}`, 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `-`, 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `-`, 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `-`, 0);
+                    constructor('td', 'td-content', `.tr-content${c}`, `-`, 0);
+                    c++;
                 }
-                constructor('tr', `tr-content${c}`, '.result-table', '', 0);
-                console.log(document.querySelector(`.tr-content${c}`));
-                constructor('td', 'td-content', `.tr-content${c}`, `${R[0].NM}`, 0);
-                constructor('td', 'td-content', `.tr-content${c}`, `${state}`, 0);
-                constructor('td', 'td-content', `.tr-content${c}`, `${R[0].DC}`, 0);
-                constructor('td', 'td-content', `.tr-content${c}`, `${date}`, 0);
-                c++;
             })
-        c++;
-
     });
 }
 
